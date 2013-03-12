c4a58607d6dec7c9b2c52a284574cae13e8c094f
DistrictEm <emily@districtem.com>
2013-03-12 18:18:47 -0400

    Initial Commit
    Committing Rails Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RGJune::Application.config.secret_token = '1f1d3143bc90a2644fb50e97ad9ebd7f389ece0a311ff83f2d599bdcb76787aab50c2d7eee4949b7aa5edc9c1b27c38bcbefddcf31a8893fcb01ff20ddf371f4'
